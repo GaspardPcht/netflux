d7b022c9026188fdcc29e866713e1fd4dc5a8f86
Gaspard <Your email>
2024-08-31 17:22:31 +0200

    change title and icon page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,12 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>Netflux</title>
+        <link rel="icon" href="/logo.png" />
       </Head>
       <Component {...pageProps} />
     </>
